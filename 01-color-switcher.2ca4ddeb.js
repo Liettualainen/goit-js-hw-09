654176f5d13872c8839162df3f24abd34b5ef040
Liettualainen <105230629+Liettualainen@users.noreply.github.com>
2022-10-24 19:16:31 +0000

    Deploying to gh-pages from @ Liettualainen/goit-js-hw-09@571ca15416afef53244edc2c53f63077387ca5a2 🚀

--- 01-color-switcher.2ca4ddeb.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");let o=null;t.addEventListener("click",(()=>{o=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(o),e.disabled=!0,t.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.2ca4ddeb.js.map
